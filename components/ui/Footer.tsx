3a4895fba770ff159766ae3efa3150c660b3a9e2
baybilon <baybilon_iqbal@hotmail.com>
2024-09-02 17:17:16 +0700

    changes

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -11,12 +11,13 @@ const Footer = () => {
                 <h1 className='lg:heading lg:max-w-[45vw] text-3xl text-center'>
                     Ready to take <span className='text-pelorous-400'>Your</span> digital presence to the next level
                 </h1>
-                <p className='text-pelorous-300 md:mt-10 my-5 text-center lg:text-md text-sm'>
+                <p className='text-pelorous-300 dark:text-pelorous-default md:mt-10 my-5 text-center lg:text-md text-sm'>
                     Reach out to me today and let&apos;s discuss how I can help you
                     achieve your goals.  
                 </p>
                 <a href='mailto:iqbal.py@outlook.com'>
                     <MagicButton
+                    className='text-black dark:text-white'
                     title='Email me'
                     icon={<FaLocationArrow />}
                     position='right'
@@ -27,7 +28,7 @@ const Footer = () => {
                 <p className='md:text-base text-sm md:font-normal font-light'>Copyright © {new Date().getFullYear()} Iqbal.py </p>
                 <div className='flex items-center md:gap-3 gap-3 md:mt-0 mt-5'>
                     {socialButton.map((social) => (
-                        <div key={social.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg backdrop-saturate-180 bg-opacity-75 bg-neutral-900 rounded-lg border border-neutral-800'>
+                        <div key={social.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg backdrop-saturate-180 text-white dark:bg-neutral-900 bg-pelorous-default rounded-lg border dark:border-neutral-800'>
                            <a href={social.link}>
                           <social.icon/>
                           </a>
